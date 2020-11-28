92770d9684883891ad0f2634a9b4303106313ebb
“akheel-s” <akheelmshaik@gmail.com>
2020-11-28 15:43:06 -0800

    Switched preset to drop down

--- src/Module/components/ModulePresets.vue
+++ src/Module/components/ModulePresets.vue
@@ -2,70 +2,12 @@
   <v-container class="module-outcomes">
     <div class="module-outcomes__container">
       <!-- <v-divider class="presets__divider"></v-divider> -->
-      <div class="presets__section-title">General</div>
+      <div class="presets__section-title">Maximum Team Members</div>
+      <v-select :items="items" filled dense outlined label="Maximum Team Members"></v-select>
       <!-- POST-ACTIVITY REFLECTION -->
-      <v-text-field placeholder="Max number of team members" outlined hide-details></v-text-field>
       <div class="presets__reflection">
         <v-btn class="presets__reflection-buttons" small depressed outlined>Save</v-btn>
       </div>
-      <!-- <v-divider class="presets__divider"></v-divider> -->
-      <!-- If activity is required, show button below, make tooltip show up while disabled, right now tooltip doesn't show up -->
-      <!-- <div class="presets__section-title">General</div>
-      <div class="presets__nopresets">No tweaking necessary</div> -->
-      <v-divider class="presets__divider"></v-divider>
-      <div class="presets__section-title">Instructions</div>
-      <Instruct v-model="setupInstructions" />
-      <v-divider class="presets__divider"></v-divider>
-      <div class="presets__section-title">Defaults</div>
-
-      <v-select :items="group" label="What activity group does this belong to?" outlined></v-select>
-      <v-select
-        :items="required"
-        label="Is this activity required for participants to complete?"
-        outlined
-      ></v-select>
-
-      <!-- <v-select
-        :items="lockOrder"
-        label="Lock activity group and placement order?"
-        outlined
-      ></v-select> -->
-      <v-select :items="deliverable" label="Is this a deliverable?" outlined></v-select>
-      <!-- <v-select
-        :items="accessibility"
-        label="Make this activity accessible to participants anytime?"
-        outlined
-      ></v-select> -->
-      <v-select
-        :items="endEarly"
-        label="Allow participants to end program early after completion of this activity?"
-        outlined
-      ></v-select>
-      <!-- POST-ACTIVITY REFLECTION -->
-      <!-- <v-text-field
-        label="Post-Activity Reflection"
-        placeholder="Now that you know the scope of the project, provide a summary of your interpretation."
-        outlined
-        hide-details
-      ></v-text-field>
-      <div class="presets__reflection">
-        <v-btn class="presets__reflection-buttons" small depressed outlined>Save</v-btn>
-        <v-btn class="presets__reflection-buttons" small depressed outlined>Preview</v-btn>
-      </div> -->
-      <v-divider class="presets__divider"></v-divider>
-      <!-- If activity is required, show button below, make tooltip show up while disabled, right now tooltip doesn't show up -->
-      <!-- <div>
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="red" disabled v-bind="attrs" v-on="on">Delete Activity</v-btn>
-          </template>
-          <span>Required activities cannot be deleted</span>
-        </v-tooltip>
-      </div> -->
-      <!-- Delete break when the two delete buttons above and below have been integrated as one solution -->
-      <br />
-      <!-- If activity is optional, show button below -->
-      <div><v-btn color="red" dark depressed>Delete Activity</v-btn></div>
     </div>
   </v-container>
 </template>
@@ -91,6 +33,7 @@ export default {
         'Yes',
         'No'
       ],
+      items: ['1', '2', '3', '4', '5', '6', '7'],
       endEarly: [
         'Creator has not allowed participants to end early after this activity',
         'Creator has allow end early option only at preset order placement',
@@ -133,7 +76,7 @@ export default {
     font-size: 25px;
     font-weight: 800;
     text-align: center;
-    margin-bottom: 20px;
+    margin-bottom: 35px;
   }
 
   &__nopresets {
