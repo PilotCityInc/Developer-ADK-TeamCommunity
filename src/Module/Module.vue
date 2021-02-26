cadc2e62d79db9e15b55b90dd0afc83151ddbfcc
Derick Lee <dericklee@pilotcity.com>
2021-02-26 12:39:02 -0800

    changing color

--- src/Module/Module.vue
+++ src/Module/Module.vue
@@ -305,7 +305,7 @@ export default defineComponent({
         ['#ae90b0', '#f79961', '#000000']
       ],
       // ENTER ACTIVITY COLOR
-      selectedColor: '#fec34b'
+      selectedColor: '#6eba80'
     });
     const getColor = computed(() => {
       return color.selectedColor.substring(0, 7);
