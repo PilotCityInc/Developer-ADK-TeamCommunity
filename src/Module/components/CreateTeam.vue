10bc635838ca2cee6129a18654c78b93359f8723
Derick Lee <dericklee@pilotcity.com>
2021-03-22 00:34:33 -0700

    1.0.28

--- src/Module/components/CreateTeam.vue
+++ src/Module/components/CreateTeam.vue
@@ -1,73 +1,76 @@
 <template>
   <div>
     <div class="headline font-weight-black mb-6 mt-6">Create</div>
-    <div class="module-default__log-text mb-5 mt-5">
-      <v-text-field
-        v-model="name"
-        :disabled="userType === 'stakeholder'"
-        rounded
-        class="module-default__text-field"
-        label="Create new team"
-        placeholder="Enter new team name"
-        outlined
-      ></v-text-field>
+    <div class="d-flex flex-row justify-start mt-5 mb-5">
+      <div>
+        <v-text-field
+          v-model="name"
+          :disabled="userType === 'stakeholder'"
+          rounded
+          class="module-default__text-field"
+          label="Create new team"
+          placeholder="Enter new team name"
+          outlined
+        ></v-text-field>
+      </div>
+      <div>
+        <v-dialog v-model="createTeamDialog" persistent max-width="450px">
+          <template #activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              rounded
+              class="module-default__log-btn"
+              depressed
+              outlined
+              :disabled="name.length === 0 || userType === 'stakeholder'"
+              :ripple="false"
+              v-on="on"
+              >Create Team</v-btn
+            >
+          </template>
 
-      <v-dialog v-model="createTeamDialog" persistent max-width="450px">
-        <template #activator="{ on, attrs }">
-          <v-btn
-            v-bind="attrs"
-            rounded
-            class="module-default__log-btn"
-            depressed
-            outlined
-            :disabled="name.length === 0 || userType === 'stakeholder'"
-            :ripple="false"
-            v-on="on"
-            >Create Team</v-btn
-          >
-        </template>
+          <v-card>
+            <v-card-title class="d-flex flex-column">
+              <div class="overline font-weight-bold">Create password for new team members</div>
+            </v-card-title>
 
-        <v-card>
-          <v-card-title class="d-flex flex-column">
-            <div class="overline font-weight-bold">Create password for new team members</div>
-          </v-card-title>
+            <v-divider></v-divider>
 
-          <v-divider></v-divider>
+            <v-container class="d-flex justify-center">
+              <div class="d-flex flex-column justify-center">
+                <v-text-field
+                  v-model="password"
+                  :readonly="userType === 'stakeholder'"
+                  class="justify-center ma-2"
+                  x-large
+                  rounded
+                  label="Enter password"
+                  outlined
+                  hide-details
+                ></v-text-field>
+                <v-btn
+                  class="ma-2"
+                  x-large
+                  :dark="password.length > 0"
+                  rounded
+                  depressed
+                  :disabled="password.length === 0 || userType === 'stakeholder'"
+                  @click="createTeam"
+                  >Set Password</v-btn
+                >
 
-          <v-container class="d-flex justify-center">
-            <div class="d-flex flex-column justify-center">
-              <v-text-field
-                v-model="password"
-                :readonly="userType === 'stakeholder'"
-                class="justify-center ma-2"
-                x-large
-                rounded
-                label="Enter password"
-                outlined
-                hide-details
-              ></v-text-field>
-              <v-btn
-                class="ma-2"
-                x-large
-                :dark="password.length > 0"
-                rounded
-                depressed
-                :disabled="password.length === 0 || userType === 'stakeholder'"
-                @click="createTeam"
-                >Set Password</v-btn
-              >
+                <!-- <div class="d-flex headline justify-center mt-6 mb-6">Or</div>
 
-              <!-- <div class="d-flex headline justify-center mt-6 mb-6">Or</div>
+                        <v-btn class="ma-2" x-large dark rounded depressed>Request to Join</v-btn> -->
 
-                      <v-btn class="ma-2" x-large dark rounded depressed>Request to Join</v-btn> -->
-
-              <div class="d-flex justify-center mt-3 mb-3">
-                <v-btn icon @click="createTeamDialog = false"><v-icon>mdi-close</v-icon></v-btn>
+                <div class="d-flex justify-center mt-3 mb-3">
+                  <v-btn icon @click="createTeamDialog = false"><v-icon>mdi-close</v-icon></v-btn>
+                </div>
               </div>
-            </div>
-          </v-container>
-        </v-card>
-      </v-dialog>
+            </v-container>
+          </v-card>
+        </v-dialog>
+      </div>
     </div>
   </div>
 </template>
